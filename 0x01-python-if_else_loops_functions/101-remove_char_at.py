00c5c02cb55eb6ab8126864a83554cb01726d9ca
soliye <soliy@gmail.com>
2022-06-28 03:59:04 -0700

    task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     if n >= 0:
-        newstr = str[:n] + str[n + 1:]
+        return ('{}{}'.format(str[:n], str[n+1:]))
     else:
-        newstr = str
-    return (newstr)
-
+        return str
