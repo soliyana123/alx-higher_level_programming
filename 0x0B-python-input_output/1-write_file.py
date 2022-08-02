05b65501e062981418aa2e98cf433c7f239bb19c
soliye <soliy@gmail.com>
2022-08-02 02:03:24 -0700

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""writes a string to text file (UTF8)"""
+"""
+Writes a string to a text file (UTF8)
+"""
+
 
 def write_file(filename="", text=""):
-    """writes a string to text file (UTF8)"""
-       with open(filename, 'w', encoding='utf-8') as f:
-           return (f.write(text))
+    """
+    Writes a string to a text file (UTF8)
+    """
+    with open(filename, mode="w", encoding="utf-8") as m_file:
+        char = m_file.write(text)
+        return char
