f08a27496351b71f2aebf24800a07de070696967
soliye <soliy@gmail.com>
2022-08-02 01:23:38 -0700

    str

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" program that return an object reprsented by json string """
+import json
+
+def from_json_string(my_str):
+    """function that return an object reprsented by json string"""
+
+     return (json.loads(my_str)
