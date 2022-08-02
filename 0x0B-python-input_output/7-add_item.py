3e0919b405f3a596de3a6b8cc5d756b695d5e7e8
soliye <soliy@gmail.com>
2022-08-02 03:43:16 -0700

    item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""
-Create JSON from argv
-"""
-
-
-from sys import argv
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+""" Program that Load, add, save all arguments to a Python list,
+and then save them to a file """
+import sys
+import os
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
 
 if __name__ == "__main__":
-    try:
-        json_list = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        json_list = []
+    data = "add_item.json"
 
-    for x in range(1, len(argv)):
-        json_list.append(argv[x])
-    save_to_json_file(json_list, "add_item.json")
+    if os.path.isfile(data):
+        my_list = load_from_json_file(data)
+    else:
+        my_list = []
+
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
+    save_to_json_file(my_list, data)
