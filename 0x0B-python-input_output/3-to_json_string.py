5684135a484c9870deb06339e5043435710d55af
soliye <soliy@gmail.com>
2022-08-02 02:09:07 -0700

    task3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""function that returns JSON representation"""
+""" function that returns JSON representation """
 import json
 
 
 def to_json_string(my_obj):
-    """function that returns JSON representation"""
+    """ function that returns JSON representation """
      return (json.dumps(my_obj))
