09358edd3fad8b098d7c900444c50acb6989729c
soliye <soliy@gmail.com>
2022-08-02 05:05:00 -0700

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" Program that Load, add, save all arguments to a Python list,
-and then save them to a file """
-import sys
-import os
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+"""
+Script that adds all arguments to a Python list, and then saves them to a file
+"""
 
+from sys import argv
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-if __name__ == "__main__":
-    data = "add_item.json"
+filename = "add_item.json"
 
-    if os.path.isfile(data):
-        my_list = load_from_json_file(data)
-    else:
-        my_list = []
+try:
+    json_list = load_from_json_file(filename)
+except:
+    json_list = []
 
-    for i in range(1, len(sys.argv)):
-        my_list.append(sys.argv[i])
-    save_to_json_file(my_list, data)
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, filename)
