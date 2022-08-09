31791930a916cb39353a2092e3e89910501bece6
soliye <soliy@gmail.com>
2022-08-09 01:27:53 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Base class of all other classes in this project"""
-
+import json
 class Base:
     """ first class Base """
     __nb_objects = 0
@@ -12,3 +12,13 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = self.__nb_objects
+           
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)       
