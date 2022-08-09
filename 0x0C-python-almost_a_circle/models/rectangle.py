0a37069fddad78d087f0fd8122347dcdab43d40f
soliye <soliy@gmail.com>
2022-08-09 04:10:13 -0700

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,59 +20,62 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """ Retriebe the width of rectangle """
-        return (self.__width)
+        """ Width getter """
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """ set passet private attribute of width """
-        if (type(value) is not int):
+        """ width setter """
+        if  not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if (value <= 0):
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
-        """ Retrieve the height of rectangle """
-        return (self.__height)
+        """ Height getter """
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """ set passed private attribute of height """
-        if (type(value) is not int):
+        """ height setter """
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if (value <= 0):
+        if value <= 0:
             raise ValueError("height must be > 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     @property
     def x(self):
-        """ Retrieve private instance attribute x """
-        return (self.__x)
+        """ x getter """
+        return self.__x
 
     @x.setter
     def x(self, value):
-        """ set private instance attribute x """
-        if (type(value) is not int):
+        """ x setter """
+        if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        if (value < 0):
+        if  value < 0:
             raise ValueError("x must be >= 0")
-        self.__x = value
+        else:
+            self.__x = value
 
     @property
     def y(self):
-        """ Retrieve private instance sttribute y """
-        return (self.__y)
+        """ y getter """
+        return self.__y
 
     @y.setter
     def y(self, value):
-        """ set private instance attribute y """
-        if (type(value) is not int):
+        """ y setter """
+        if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        if (value < 0):
+        if value < 0:
             raise ValueError("y must be >= 0")
-        self.__y = value
+        else:
+            self.__y = value
 
      def area(self):
         """
