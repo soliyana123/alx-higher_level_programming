b37b830d92946007a8286b3ebdeb1ab3f5b9edaf
soliyana123 <asefukassaw2@gmail.com>
2022-08-10 00:05:45 -0700

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,24 +1,8 @@
-
 #!/usr/bin/python3
 """
-Contains the "Rectangle" class
+Rectangle class
 """
-class Base:
-    """
-    New class base
-    """
-
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """
-        new var
-        """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = self.__nb_objects
+from models.base import Base
 
 
 
