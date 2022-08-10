7b4e88de7eec5dcd221b61986c1590336d4264fe
soliyana123 <asefukassaw2@gmail.com>
2022-08-09 23:51:21 -0700

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Rectangle class
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        var for the rec
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
