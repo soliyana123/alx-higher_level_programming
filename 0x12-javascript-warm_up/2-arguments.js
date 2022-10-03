4ee583bb72113f25436589288f7273a04c96174a
soliye <soliy@gmail.com>
2022-10-03 11:15:18 -0700

    argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const newVar = process.argv.length;
-if (newVar === 2) {
+// cript that prints a message depending of the number of arguments passed
+const args = process.argv;
+if (args.length === 2) {
   console.log('No argument');
-} else if (newVar === 3) {
+} else if (args.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
