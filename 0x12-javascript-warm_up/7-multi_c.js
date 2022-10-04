40c50cc2a6f52177d22bca8a30756d2ac8286efd
soliye <soliy@gmail.com>
2022-10-03 17:15:16 -0700

    add

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-const newVar = parseInt(process.argv[2]);
-
-if (isNaN(newVar)) {
+// script that prints x times “C is fun”
+const arg = process.argv.slice(2);
+const argInt = parseInt(arg[0]);
+if (isNaN(argInt)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let x = 0; x < newVar; x++) {
+  for (let i = 0; i < argInt; i++) {
     console.log('C is fun');
   }
 }
