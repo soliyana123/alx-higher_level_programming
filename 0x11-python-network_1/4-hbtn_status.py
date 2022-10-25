15d0afbbd45acb708151e61fc67710cb2278fef0
soliye <soliy@gmail.com>
2022-10-25 01:07:24 -0700

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""Check status"""
+""" Program that fetch an URL with requests package """
 import requests
 
 
-def status():
-    """status"""
-    result = requests.get("https://intranet.hbtn.io/status")
-
-    print("Body response:")
-    print("\t- type: {}".format(type(result.text)))
-    print("\t- content: {}".format(result.text))
-
 if __name__ == "__main__":
-    status()
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
